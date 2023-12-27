985808b168107ca18913908e12f30158c36b36c6
azamat <azamat.blimgotov@bk.ru>
2023-12-27 10:22:56 +0300

    пересдача

--- open and read file/open_readfile_task3.py
+++ open and read file/open_readfile_task3.py
@@ -4,7 +4,7 @@ def line_counter(file_name):
         for line in fp:
             count += 1
     return f"\n\n{count}\n"
-
+#
 def lister(file_name):
     with open(file_name) as fs:
         file_1 = []
@@ -13,11 +13,33 @@ def lister(file_name):
             file_1.append(line)
     return file_1
 
-with open('comlicte_text', 'w') as fq:
-        fq.writelines(lister("2.txt"))
-        fq.write(line_counter("2.txt"))
-        fq.writelines(lister("1.txt"))
-        fq.write(line_counter("1.txt"))
-        fq.writelines(lister("3.txt"))
-        fq.write(line_counter("3.txt"))
+import os
+directory = "/home/azamat/Desktop/py-homework-basic-files/2.4.files/sorted"
+file_name = os.listdir(directory)
+for i in range(len(file_name)):
+    if file_name[i].endswith('txt') == False:
+        file_name -= file_name[i]
 
+file_dict = {}
+for file in file_name:
+    with open(file, encoding='utf-8') as files:
+        b = []
+        for count, line in enumerate(files):
+            b.append(line)
+            file_dict[file] = [count + 1] + b
+sorted_file = []
+
+for d in file_dict:
+    b = file_dict[d]
+    b = b[0]
+    sorted_file += [[b] + [d]]
+sorted_file.sort()
+sorted_files = []
+
+for u in sorted_file:
+    sorted_files.append(u[1])
+
+for k in sorted_files:
+    with open("complicte.txt", "a", encoding='utf-8') as fp:
+        fp.writelines(lister(k))
+        fp.write(line_counter(k))
